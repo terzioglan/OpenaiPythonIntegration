69e695fafb30136d8e4128242cd49d8fe77b7db8
terzioglan <terziogluyunus@gmail.com>
2025-11-18 13:42:13 -0500

    stdout current state

--- realtimeLocalServer.py
+++ realtimeLocalServer.py
@@ -11,23 +11,32 @@ if __name__ == "__main__":
     websocketThread = threading.Thread(target=realtimeWebsocket.runWebsocket)
     websocketThread.start()
 
+    print("Establishing local TCP connection...")
     realtimeLocalServer = Server(
         host="localhost",
         port=configuration.TCP_PORT,
         size=configuration.TCP_DATA_SIZE
         )
+    print("Done.")
 
+    print("Initializing websocket session...")
     while(not (realtimeWebsocket.sessionCreated and realtimeWebsocket.sessionUpdated)):
         print("Waiting to initialize session.")
         time.sleep(1.0)
+    print("Done.")
 
     while True:
         try:
             data = realtimeLocalServer.receive(configuration.TCP_DATA_SIZE)
+            print("Received request from local client.")
+            print("Requesting response from realtime API.")
             realtimeWebsocket.requestResponse(data["message"])
             while(realtimeWebsocket.serverResponseQueue.empty()):
                 pass
-            realtimeLocalServer.send({"message":realtimeWebsocket.serverResponseQueue.get()})
+            response = realtimeWebsocket.serverResponseQueue.get()
+            print(f"Response received: {response}")
+            print("Sending response to local client.")
+            realtimeLocalServer.send({"message" : response})
         except KeyboardInterrupt:
             break
 
